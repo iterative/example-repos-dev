6907c432ebfe0fdbbff461297993db898f4a66ea
dberenbaum <dave@iterative.ai>
2021-04-13 13:52:05 -0400

    upgrade torchvision

--- train.py
+++ train.py
@@ -7,15 +7,6 @@ import torch.nn.functional as F
 import torchvision
 
 
-# See https://github.com/pytorch/vision/issues/3549.
-torchvision.datasets.MNIST.resources = [
-            ('https://ossci-datasets.s3.amazonaws.com/mnist/train-images-idx3-ubyte.gz', 'f68b3c2dcbeaaa9fbdd348bbdeb94873'),
-            ('https://ossci-datasets.s3.amazonaws.com/mnist/train-labels-idx1-ubyte.gz', 'd53e105ee54ea40749a09fcbcd1e9432'),
-            ('https://ossci-datasets.s3.amazonaws.com/mnist/t10k-images-idx3-ubyte.gz', '9fb629c4189551a2d022fa330f9573f3'),
-            ('https://ossci-datasets.s3.amazonaws.com/mnist/t10k-labels-idx1-ubyte.gz', 'ec29112dd5afa0611ce80d1b7f02629c')
-        ]
-
-
 class ConvNet(torch.nn.Module):
     """Toy convolutional neural net."""
     def __init__(self):
