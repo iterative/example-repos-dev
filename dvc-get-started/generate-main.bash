44090b9ae8e67c8de44af020862500a863e0f2a4
github@emresult.com <github@emresult.com>
2021-05-17 10:44:45 +0000

    Added tag_tick to remove repeats and fixed shell check warnings

--- dvc-get-started/generate-main.bash
+++ dvc-get-started/generate-main.bash
@@ -11,36 +11,26 @@ set -eux
 git init
 git checkout -b main
 cp $HERE/code-main/README.md .
+
+tag_tick
 git add .
-TAG_TIME=$(( ${TAG_TIME} + ${STEP_TIME} ))
-GIT_AUTHOR_DATE=${TAG_TIME} \
-GIT_COMMITTER_DATE=${TAG_TIME} \
 git commit -m  "Initialize Git repository"
-GIT_AUTHOR_DATE=${TAG_TIME} \
-GIT_COMMITTER_DATE=${TAG_TIME} \
 git tag -a "git-init" -m "Git initialized."
 
 
+tag_tick
 dvc init
-TAG_TIME=$(( ${TAG_TIME} + ${STEP_TIME} ))
-GIT_AUTHOR_DATE=${TAG_TIME} \
-GIT_COMMITTER_DATE=${TAG_TIME} \
 git commit -m "Initialize DVC project"
-GIT_AUTHOR_DATE=${TAG_TIME} \
-GIT_COMMITTER_DATE=${TAG_TIME} \
 git tag -a "dvc-init" -m "DVC initialized."
 
 # Remote active on this env only, for writing to HTTP redirect below.
 dvc remote add -d --local storage s3://dvc-public/remote/get-started
 # Actual remote for generated project (read-only). Redirect of S3 bucket above.
 dvc remote add -d storage https://remote.dvc.org/get-started
+
+tag_tick
 git add .
-TAG_TIME=$(( ${TAG_TIME} + ${STEP_TIME} ))
-GIT_AUTHOR_DATE=${TAG_TIME} \
-GIT_COMMITTER_DATE=${TAG_TIME} \
 git commit -m "Configure default remote"
-GIT_AUTHOR_DATE=${TAG_TIME} \
-GIT_COMMITTER_DATE=${TAG_TIME} \
 git tag -a "config-remote" -m "Read-only remote storage configured."
 
 test -d data || mkdir data
@@ -48,27 +38,20 @@ test -d data || mkdir data
 dvc import https://github.com/iterative/dataset-registry \
            mnist/raw -o data/raw
 
+tag_tick
 git add data/raw.dvc data/.gitignore
-TAG_TIME=$(( ${TAG_TIME} + ${STEP_TIME} ))
-GIT_AUTHOR_DATE=${TAG_TIME} \
-GIT_COMMITTER_DATE=${TAG_TIME} \
 git commit -m "Add raw MNIST data"
-GIT_AUTHOR_DATE=${TAG_TIME} \
-GIT_COMMITTER_DATE=${TAG_TIME} \
 git tag -a "import-mnist-data" -m "MNIST data file added."
 dvc push
 
-cp -r ${HERE}/code-main/src .
-cp ${HERE}/code-main/requirements.txt .
-cp ${HERE}/code-main/params.yaml .
-pip install -r ${REPO_PATH}/requirements.txt
+cp -r "${HERE}"/code-main/src .
+cp "${HERE}"/code-main/requirements.txt .
+cp "${HERE}"/code-main/params.yaml .
+pip install -r "${REPO_PATH}"/requirements.txt
+
+tag_tick
 git add .
-TAG_TIME=$(( ${TAG_TIME} + ${STEP_TIME} ))
-GIT_AUTHOR_DATE=${TAG_TIME} \
-GIT_COMMITTER_DATE=${TAG_TIME} \
 git commit -m "Add source code files to repo"
-GIT_AUTHOR_DATE=${TAG_TIME} \
-GIT_COMMITTER_DATE=${TAG_TIME} \
 git tag -a "source-code" -m "Source code added."
 
 dvc stage add -n prepare \
@@ -82,14 +65,10 @@ dvc stage add -n prepare \
 
 dvc repro prepare 
 
+tag_tick
 git add data/.gitignore dvc.yaml dvc.lock
-TAG_TIME=$(( ${TAG_TIME} + ${STEP_TIME} ))
-GIT_AUTHOR_DATE=${TAG_TIME} \
-GIT_COMMITTER_DATE=${TAG_TIME} \
 git commit -m "Create data preparation stage"
 dvc push
-GIT_AUTHOR_DATE=${TAG_TIME} \
-GIT_COMMITTER_DATE=${TAG_TIME} \
 git tag -a "prepare" -m "First pipeline stage (data preparation) created."
 
 dvc stage add -n preprocess \
@@ -107,12 +86,8 @@ dvc stage add -n preprocess \
 dvc repro preprocess
 dvc push
 git add data/.gitignore dvc.yaml dvc.lock
-TAG_TIME=$(( ${TAG_TIME} + ${STEP_TIME} ))
-GIT_AUTHOR_DATE=${TAG_TIME} \
-GIT_COMMITTER_DATE=${TAG_TIME} \
+tag_tick
 git commit -m "Second pipeline stage (preprocessing) created"
-GIT_AUTHOR_DATE=${TAG_TIME} \
-GIT_COMMITTER_DATE=${TAG_TIME} \
 git tag -a "preprocess" -m "Second pipeline stage (data preprocessing) created."
 
 mkdir models
@@ -138,15 +113,11 @@ dvc stage add -n train \
 
 # TODO: We may need to add some `dvc plots modify` commands here!
 
+tag_tick
 dvc repro train
 git add .gitignore dvc.yaml dvc.lock models/.gitignore
-TAG_TIME=$(( ${TAG_TIME} + ${STEP_TIME} ))
-GIT_AUTHOR_DATE=${TAG_TIME} \
-GIT_COMMITTER_DATE=${TAG_TIME} \
 git commit -m "Created training stage"
 dvc push
-GIT_AUTHOR_DATE=${TAG_TIME} \
-GIT_COMMITTER_DATE=${TAG_TIME} \
 git tag -a "training" -m "Training stage created."
 
 dvc stage add -n evaluate \
@@ -156,13 +127,9 @@ dvc stage add -n evaluate \
               python3 src/evaluate.py
 dvc repro evaluate
 
+tag_tick
 git add .gitignore dvc.yaml dvc.lock metrics.json
-TAG_TIME=$(( ${TAG_TIME} + ${STEP_TIME} ))
-GIT_AUTHOR_DATE=${TAG_TIME} \
-GIT_COMMITTER_DATE=${TAG_TIME} \
 git commit -m "Create evaluation stage"
 dvc push
-GIT_AUTHOR_DATE=${TAG_TIME} \
-GIT_COMMITTER_DATE=${TAG_TIME} \
 git tag -a "evaluation" -m "Evaluation stage created."
 
