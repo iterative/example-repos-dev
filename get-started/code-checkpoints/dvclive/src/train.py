a4a066ab624fe922c6cce72b174d2833515f1368
Emre Şahin <github@emresult.com>
2021-05-25 13:22:18 +0300

    Callback cleanup

--- get-started/code-checkpoints/dvclive/src/train.py
+++ get-started/code-checkpoints/dvclive/src/train.py
@@ -10,50 +10,12 @@ MODEL_FILE = "models/model.h5"
 
 class DVCLiveCallback(tf.keras.callbacks.Callback):
 
-    def on_train_begin(self, logs=None):
-        pass
-
-    def on_train_end(self, logs=None):
-        pass
-
-    def on_epoch_begin(self, epoch, logs=None):
-        pass
-
     def on_epoch_end(self, epoch, logs=None):
         logs = logs or {}
         for metric, value in logs.items():
             dvclive.log(metric, value)
         dvclive.next_step()
 
-    def on_test_begin(self, logs=None):
-        pass
-
-    def on_test_end(self, logs=None):
-        pass
-
-    def on_predict_begin(self, logs=None):
-        pass
-
-    def on_predict_end(self, logs=None):
-        pass
-
-    def on_train_batch_begin(self, batch, logs=None):
-        pass
-
-    def on_train_batch_end(self, batch, logs=None):
-        pass
-
-    def on_test_batch_begin(self, batch, logs=None):
-        pass
-
-    def on_test_batch_end(self, batch, logs=None):
-        pass
-
-    def on_predict_batch_begin(self, batch, logs=None):
-        pass
-
-    def on_predict_batch_end(self, batch, logs=None):
-        pass
 
 def load_npz_data(filename):
     npzfile = np.load(filename)
