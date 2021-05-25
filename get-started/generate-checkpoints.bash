c68f6b852e9930067a342eb39f8f31ba1ecb11db
Emre Şahin <github@emresult.com>
2021-05-25 12:15:50 +0300

    add data import to generate script

--- get-started/generate-checkpoints.bash
+++ get-started/generate-checkpoints.bash
@@ -10,23 +10,33 @@ pushd "${REPO_PATH}"
 virtualenv -p python3 .venv
 export VIRTUAL_ENV_DISABLE_PROMPT=true
 source .venv/bin/activate
-echo '.venv/' > .gitignore
-
-pip install 'dvc[all]'
 
 git init
 git checkout -b main
-
-cp -r "${HERE}"/code-checkpoints/00-common/* "${REPO_PATH}"/
+dvc init
+# Remote active on this env only, for writing to HTTP redirect below.
+dvc remote add -d --local storage s3://dvc-public/remote/get-started
+# Actual remote for generated project (read-only). Redirect of S3 bucket above.
+dvc remote add -d storage https://remote.dvc.org/get-started
 tag_tick
+cp -ar "${HERE}"/code-checkpoints/00-common/. "${REPO_PATH}"/
 git add "${REPO_PATH}/"*
-git commit -m "Added common files for checkpoints"
-git tag -a "checkpoints-base" -m "checkpoints baseline"
+git commit -m "Initialized DVC and Configured Remote"
+git tag -a "init" -m "Initialized DVC and Remote"
 
-# We'll use the main code and modify the parts of it
+test -d data/fashion-mnist || mkdir -p data/fashion-mnist
 
-cp -r "${HERE}"/code-checkpoints/00-common/* "${REPO_PATH}"/
-cp -r "${HERE}"/code-checkpoints/basic/* "${REPO_PATH}"/
+dvc import https://github.com/iterative/dataset-registry \
+           fashion-mnist/raw -o data/fashion-mnist/raw
+
+tag_tick
+git add data/fashion-mnist/raw.dvc data/fashion-mnist/.gitignore
+git commit -m "Add Fashion-MNIST data"
+git tag -a "data" -m "Fashion-MNIST data file added."
+dvc push
+
+cp -arf "${HERE}"/code-checkpoints/00-common/. "${REPO_PATH}"/
+cp -arf "${HERE}"/code-checkpoints/basic/* "${REPO_PATH}"/
 pip install -r "${REPO_PATH}"/requirements.txt
 
 tag_tick
@@ -35,8 +45,8 @@ git commit -m "Added 'checkpoint: true' to the params file"
 git tag -a "basic" -m "checkpoint: true example"
 
 
-cp -r "${HERE}"/code-checkpoints/00-common/* "${REPO_PATH}"/
-cp -r "${HERE}"/code-checkpoints/signal-file/* "${REPO_PATH}"/
+cp -arf "${HERE}"/code-checkpoints/00-common/. "${REPO_PATH}"/
+cp -arf "${HERE}"/code-checkpoints/signal-file/* "${REPO_PATH}"/
 
 tag_tick
 git add .
@@ -44,16 +54,16 @@ git commit -m "Modified train.py for signal files"
 git tag -a "signal-file" -m "Checkpoints: Signal file example"
 
 
-cp -r "${HERE}"/code-checkpoints/00-common/* "${REPO_PATH}"/
-cp -r "${HERE}"/code-checkpoints/python-api/* "${REPO_PATH}"/
+cp -arf "${HERE}"/code-checkpoints/00-common/. "${REPO_PATH}"/
+cp -arf "${HERE}"/code-checkpoints/python-api/* "${REPO_PATH}"/
 tag_tick
 git add .
 git commit -m "Using make_checkpoint() in the callback"
 git tag -a "python-api" -m "Checkpoints: Python API example"
 
 
-cp -r "${HERE}"/code-checkpoints/00-common/* "${REPO_PATH}"/
-cp -r "${HERE}"/code-checkpoints/dvclive/* "${REPO_PATH}"/
+cp -arf "${HERE}"/code-checkpoints/00-common/. "${REPO_PATH}"/
+cp -arf "${HERE}"/code-checkpoints/dvclive/* "${REPO_PATH}"/
 
 tag_tick
 git add .
