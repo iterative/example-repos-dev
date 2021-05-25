4dfc901ff68bdd5a1b6a3dd58a2b84658d4dda2e
Emre Şahin <github@emresult.com>
2021-05-25 11:39:22 +0300

    Cleanup Callback Class for signal-file

--- get-started/code-checkpoints/signal-file/src/train.py
+++ get-started/code-checkpoints/signal-file/src/train.py
@@ -24,48 +24,10 @@ class DVCCheckpointsCallback(tf.keras.callbacks.Callback):
                 # Wait 10 milliseconds
                 time.sleep(0.01)
 
-    def on_train_begin(self, logs=None):
-        pass
-
-    def on_train_end(self, logs=None):
-        pass
-
-    def on_epoch_begin(self, epoch, logs=None):
-        pass
-
     def on_epoch_end(self, epoch, logs=None):
         if (epoch % self.frequency) == 0:
             dvc_signal(self)
 
-    def on_test_begin(self, logs=None):
-        pass
-
-    def on_test_end(self, logs=None):
-        pass
-
-    def on_predict_begin(self, logs=None):
-        pass
-
-    def on_predict_end(self, logs=None):
-        pass
-
-    def on_train_batch_begin(self, batch, logs=None):
-        pass
-
-    def on_train_batch_end(self, batch, logs=None):
-        pass
-
-    def on_test_batch_begin(self, batch, logs=None):
-        pass
-
-    def on_test_batch_end(self, batch, logs=None):
-        pass
-
-    def on_predict_batch_begin(self, batch, logs=None):
-        pass
-
-    def on_predict_batch_end(self, batch, logs=None):
-        pass
 
 def load_npz_data(filename):
     npzfile = np.load(filename)
