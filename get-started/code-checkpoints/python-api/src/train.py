c9766f85864c0a8ce6fcf1429b0d3ea68f8f58e2
Emre Şahin <github@emresult.com>
2021-05-25 11:43:46 +0300

    Callback cleanup for python-api

--- get-started/code-checkpoints/python-api/src/train.py
+++ get-started/code-checkpoints/python-api/src/train.py
@@ -13,48 +13,9 @@ class DVCCheckpointsCallback(tf.keras.callbacks.Callback):
     def __init__(self, frequency = 1):
         self.frequency = frequency
 
-    def on_train_begin(self, logs=None):
-        pass
-
-    def on_train_end(self, logs=None):
-        pass
-
-    def on_epoch_begin(self, epoch, logs=None):
-        pass
-
     def on_epoch_end(self, epoch, logs=None):
         if (epoch % self.frequency) == 0:
             make_checkpoint()
-
-    def on_test_begin(self, logs=None):
-        pass
-
-    def on_test_end(self, logs=None):
-        pass
-
-    def on_predict_begin(self, logs=None):
-        pass
-
-    def on_predict_end(self, logs=None):
-        pass
-
-    def on_train_batch_begin(self, batch, logs=None):
-        pass
-
-    def on_train_batch_end(self, batch, logs=None):
-        pass
-
-    def on_test_batch_begin(self, batch, logs=None):
-        pass
-
-    def on_test_batch_end(self, batch, logs=None):
-        pass
-
-    def on_predict_batch_begin(self, batch, logs=None):
-        pass
-
-    def on_predict_batch_end(self, batch, logs=None):
-        pass
 
 def load_npz_data(filename):
     npzfile = np.load(filename)
