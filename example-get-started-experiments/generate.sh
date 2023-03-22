83db6420493744d6f3261f20f321bf133f522825
dberenbaum <dave@iterative.ai>
2023-03-22 14:08:23 -0400

    experiments: use Live(dvcyaml=True)

--- example-get-started-experiments/generate.sh
+++ example-get-started-experiments/generate.sh
@@ -106,15 +106,15 @@ dvc stage add -n train \
   -p base,train \
   -d src/train.py -d data/train_data \
   -o models/model.pkl \
-  -M results/train/metrics.json \
-  --plots-no-cache results/train/plots \
+  -O results/train/metrics.json \
+  -O results/train/plots \
   python src/train.py
 
 dvc stage add -n evaluate \
   -p base,evaluate \
   -d src/evaluate.py -d models/model.pkl -d data/test_data \
-  -M results/evaluate/metrics.json \
-  --plots-no-cache results/evaluate/plots \
+  -O results/evaluate/metrics.json \
+  -O results/evaluate/plots \
   python src/evaluate.py
 
 rm results/train/dvc.yaml
