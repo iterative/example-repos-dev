f9897924dd56dca4606d13c5a98eead89ae9db38
Leonard <leocser632@gmail.com>
2023-04-17 11:18:45 +0200

    Update src code

--- example-get-started/code/src/evaluate.py
+++ example-get-started/code/src/evaluate.py
@@ -11,21 +11,17 @@ from dvclive import Live
 from matplotlib import pyplot as plt
 
 
-EVAL_PATH = "eval"
+def evaluate(model, matrix, split, live, save_path):
+    """
+    Dump all evaluation metrics and plots for given datasets.
 
-
-if len(sys.argv) != 3:
-    sys.stderr.write("Arguments error. Usage:\n")
-    sys.stderr.write("\tpython evaluate.py model features\n")
-    sys.exit(1)
-
-model_file = sys.argv[1]
-train_file = os.path.join(sys.argv[2], "train.pkl")
-test_file = os.path.join(sys.argv[2], "test.pkl")
-
-
-def evaluate(model, matrix, split, live):
-    """Dump all evaluation metrics and plots for given datasets."""
+    Args:
+        model (sklearn.ensemble.RandomForestClassifier): Trained classifier.
+        matrix (scipy.sparse.csr_matrix): Input matrix.
+        split (str): Dataset name.
+        live (dvclive.Live): Dvclive instance.
+        save_path (str): Path to save the metrics.
+    """
     labels = matrix[:, 1].toarray().astype(int)
     x = matrix[:, 2:]
 
@@ -47,11 +43,12 @@ def evaluate(model, matrix, split, live):
     # ROC has a drop_intermediate arg that reduces the number of points.
     # https://scikit-learn.org/stable/modules/generated/sklearn.metrics.roc_curve.html#sklearn.metrics.roc_curve.
     # PRC lacks this arg, so we manually reduce to 1000 points as a rough estimate.
-    precision, recall, prc_thresholds = \
-            metrics.precision_recall_curve(labels, predictions)
+    precision, recall, prc_thresholds = metrics.precision_recall_curve(
+        labels, predictions
+    )
     nth_point = math.ceil(len(prc_thresholds) / 1000)
     prc_points = list(zip(precision, recall, prc_thresholds))[::nth_point]
-    prc_dir = os.path.join(EVAL_PATH, "prc")
+    prc_dir = os.path.join(save_path, "prc")
     os.makedirs(prc_dir, exist_ok=True)
     prc_file = os.path.join(prc_dir, f"{split}.json")
     with open(prc_file, "w") as fd:
@@ -66,36 +63,66 @@ def evaluate(model, matrix, split, live):
             indent=4,
         )
 
-
     # ... confusion matrix plot
-    live.log_sklearn_plot("confusion_matrix",
-                          labels.squeeze(),
-                          predictions_by_class.argmax(-1),
-                          name=f"cm/{split}"
-                         )
+    live.log_sklearn_plot(
+        "confusion_matrix",
+        labels.squeeze(),
+        predictions_by_class.argmax(-1),
+        name=f"cm/{split}",
+    )
 
 
-# Load model and data.
-with open(model_file, "rb") as fd:
-    model = pickle.load(fd)
+def save_importance_plot(model, feature_names, save_path):
+    """
+    Save feature importance plot.
 
-with open(train_file, "rb") as fd:
-    train, feature_names = pickle.load(fd)
+    Args:
+        model (sklearn.ensemble.RandomForestClassifier): Trained classifier.
+        feature_names (list): List of feature names.
+        save_path (str): Path to save plot.
+    """
+    fig, axes = plt.subplots(dpi=100)
+    fig.subplots_adjust(bottom=0.2, top=0.95)
+    axes.set_ylabel("Mean decrease in impurity")
 
-with open(test_file, "rb") as fd:
-    test, _ = pickle.load(fd)
+    importances = model.feature_importances_
+    forest_importances = pd.Series(importances, index=feature_names).nlargest(n=30)
+    forest_importances.plot.bar(ax=axes)
 
-# Evaluate train and test datasets.
-live = Live(os.path.join(EVAL_PATH, "live"), dvcyaml=False)
-evaluate(model, train, "train", live)
-evaluate(model, test, "test", live)
-live.make_summary()
+    fig.savefig(os.path.join(save_path, "importance.png"))
 
-# Dump feature importance image and show it with your plots.
-fig, axes = plt.subplots(dpi=100)
-fig.subplots_adjust(bottom=0.2, top=0.95)
-importances = model.feature_importances_
-forest_importances = pd.Series(importances, index=feature_names).nlargest(n=30)
-axes.set_ylabel("Mean decrease in impurity")
-forest_importances.plot.bar(ax=axes)
-fig.savefig(os.path.join(EVAL_PATH, "importance.png"))
+
+def main():
+    EVAL_PATH = "eval"
+
+    if len(sys.argv) != 3:
+        sys.stderr.write("Arguments error. Usage:\n")
+        sys.stderr.write("\tpython evaluate.py model features\n")
+        sys.exit(1)
+
+    model_file = sys.argv[1]
+    train_file = os.path.join(sys.argv[2], "train.pkl")
+    test_file = os.path.join(sys.argv[2], "test.pkl")
+
+    # Load model and data.
+    with open(model_file, "rb") as fd:
+        model = pickle.load(fd)
+
+    with open(train_file, "rb") as fd:
+        train, feature_names = pickle.load(fd)
+
+    with open(test_file, "rb") as fd:
+        test, _ = pickle.load(fd)
+
+    # Evaluate train and test datasets.
+    live = Live(os.path.join(EVAL_PATH, "live"), dvcyaml=False)
+    evaluate(model, train, "train", live, save_path=EVAL_PATH)
+    evaluate(model, test, "test", live, save_path=EVAL_PATH)
+    live.make_summary()
+
+    # Dump feature importance plot.
+    save_importance_plot(model, feature_names, save_path=EVAL_PATH)
+
+
+if __name__ == "__main__":
+    main()
--- example-get-started/code/src/prepare.py
+++ example-get-started/code/src/prepare.py
@@ -1,4 +1,3 @@
-import io
 import os
 import random
 import re
@@ -7,25 +6,20 @@ import xml.etree.ElementTree
 
 import yaml
 
-params = yaml.safe_load(open("params.yaml"))["prepare"]
 
-if len(sys.argv) != 2:
-    sys.stderr.write("Arguments error. Usage:\n")
-    sys.stderr.write("\tpython prepare.py data-file\n")
-    sys.exit(1)
+def process_posts(input_lines, fd_out_train, fd_out_test, target_tag, split):
+    """
+    Process the input lines and write the output to the output files.
 
-# Test data set split ratio
-split = params["split"]
-random.seed(params["seed"])
-
-input = sys.argv[1]
-output_train = os.path.join("data", "prepared", "train.tsv")
-output_test = os.path.join("data", "prepared", "test.tsv")
-
-
-def process_posts(fd_in, fd_out_train, fd_out_test, target_tag):
+    Args:
+        input_lines (list): List of input lines.
+        fd_out_train (file): Output file for the training data set.
+        fd_out_test (file): Output file for the test data set.
+        target_tag (str): Target tag.
+        split (float): Test data set split ratio.
+    """
     num = 1
-    for line in fd_in:
+    for line in input_lines:
         try:
             fd_out = fd_out_train if random.random() > split else fd_out_test
             attr = xml.etree.ElementTree.fromstring(line).attrib
@@ -43,9 +37,42 @@ def process_posts(fd_in, fd_out_train, fd_out_test, target_tag):
             sys.stderr.write(f"Skipping the broken line {num}: {ex}\n")
 
 
-os.makedirs(os.path.join("data", "prepared"), exist_ok=True)
+def main():
+    params = yaml.safe_load(open("params.yaml"))["prepare"]
 
-with io.open(input, encoding="utf8") as fd_in:
-    with io.open(output_train, "w", encoding="utf8") as fd_out_train:
-        with io.open(output_test, "w", encoding="utf8") as fd_out_test:
-            process_posts(fd_in, fd_out_train, fd_out_test, "<r>")
+    if len(sys.argv) != 2:
+        sys.stderr.write("Arguments error. Usage:\n")
+        sys.stderr.write("\tpython prepare.py data-file\n")
+        sys.exit(1)
+
+    # Test data set split ratio
+    split = params["split"]
+    random.seed(params["seed"])
+
+    input = sys.argv[1]
+    output_train = os.path.join("data", "prepared", "train.tsv")
+    output_test = os.path.join("data", "prepared", "test.tsv")
+
+    os.makedirs(os.path.join("data", "prepared"), exist_ok=True)
+
+    input_lines = []
+    with open(input) as fd_in:
+        input_lines = fd_in.readlines()
+
+    fd_out_train = open(output_train, "w", encoding="utf-8")
+    fd_out_test = open(output_test, "w", encoding="utf-8")
+
+    process_posts(
+        input_lines=input_lines,
+        fd_out_train=fd_out_train,
+        fd_out_test=fd_out_test,
+        target_tag="<r>",
+        split=split,
+    )
+
+    fd_out_train.close()
+    fd_out_test.close()
+
+
+if __name__ == "__main__":
+    main()
