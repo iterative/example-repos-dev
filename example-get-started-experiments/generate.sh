a208212c3fd035dbba178291933f46632ec7e426
David de la Iglesia Castro <daviddelaiglesiacastro@gmail.com>
2023-07-03 08:48:11 +0000

    remove hardcoded cpu

--- example-get-started-experiments/generate.sh
+++ example-get-started-experiments/generate.sh
@@ -77,8 +77,7 @@ cp -r $HERE/code/notebooks .
 git add .
 git commit -m "Add notebook using DVCLive"
 
-pip install torch torchvision --index-url https://download.pytorch.org/whl/cpu
-pip install -r requirements.txt
+pip install -r requirements.txt --index-url https://download.pytorch.org/whl/cu118
 pip install jupyter
 jupyter nbconvert --execute 'notebooks/TrainSegModel.ipynb' --inplace
 # Apply best experiment
