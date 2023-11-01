9b6681968762d870da7e005030d9551a3b3eab94
Tibor Mach <tibor@iterative.ai>
2023-11-01 02:15:30 +0100

    tags to clearly denote model management trail start

--- example-get-started-experiments/generate.sh
+++ example-get-started-experiments/generate.sh
@@ -126,13 +126,14 @@ dvc stage add -n sagemaker \
 git add .
 tick
 git commit -m "Convert Notebook to dvc.yaml pipeline"
+git tag -a "2-get-started-model-management"
 
 
 dvc exp run
 git add .
 tick
 git commit -m "Run dvc.yaml pipeline"
-git tag -a "2-dvc-pipeline" -m "Experiment using dvc pipeline"
+git tag -a "3-dvc-pipeline" -m "Experiment run using dvc pipeline"
 tick
 gto register pool-segmentation --version v1.0.0
 gto assign pool-segmentation --version v1.0.0 --stage dev
